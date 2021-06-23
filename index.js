072c2e55909febb8c3efa4ff1837a2ace968335c
Diego <diegofernando913@gmail.com>
2021-06-22 19:20:08 -0500

    se modifican las variables principales
    se modifican las variables principales

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const { mongoose } = require('./server/database');
 
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.use(cors());
 app.use(express.json());
--- server/keys.js
+++ server/keys.js
@@ -1,7 +1,6 @@
 module.exports = {
-    URI: 'mongodb://localhost/prueba',
+    URI: process.env.MONGODB || 'mongodb://localhost/prueba',
     KEYTOKEN: 'Nodejs2021',
     URLGIFS: 'https://api.giphy.com/v1/gifs/search?',
-    KEYGIFS: 'b1xb5U31T2BDuwxAQSMqMAm1TqHPy9Jh',
-    SEARCH: 'hola'
+    KEYGIFS: 'b1xb5U31T2BDuwxAQSMqMAm1TqHPy9Jh'
 }
