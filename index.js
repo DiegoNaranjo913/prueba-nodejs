422086fae3f5295934c4cb9c27b581e09ee853ba
Diego <diegofernando913@gmail.com>
2021-06-22 20:17:56 -0500

    se cambia el nombre de la carpeta principal
    se cambia el nombre de la carpeta principal

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { mongoose } = require('./server/database');
+const { mongoose } = require('./src/database');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -12,6 +12,6 @@ app.listen(app.get('port'), () => {
     console.log('Bienvenido al puerto', app.get('port'));
 });
 
-app.use('/api/personas', require('./server/routes/personas'));
-app.use('/api/gifs', require('./server/routes/gifs'));
-app.use('/api', require('./server/routes/token'));
+app.use('/api/personas', require('./src/routes/personas'));
+app.use('/api/gifs', require('./src/routes/gifs'));
+app.use('/api', require('./src/routes/token'));
